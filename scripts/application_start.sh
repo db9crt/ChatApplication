c3eb275334942d50afa1e5d3fe07132ddee9462d
db9crt <116734267+db9crt@users.noreply.github.com>
2022-12-21 22:13:46 +0000

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/ChatApplication/deploy.log
 
-echo 'pm2 restart chatapp' >> /home/ubuntu/ChatApplication/deploy.log
-pm2 restart chatapp >> /home/ubuntu/ChatApplication/deploy.log
+echo 'pm2 restart server' >> /home/ubuntu/ChatApplication/deploy.log
+pm2 restart server >> /home/ubuntu/ChatApplication/deploy.log
