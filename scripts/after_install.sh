d001c6236fe13976cb63966511f79be77b4fd9d8
db9crt <116734267+db9crt@users.noreply.github.com>
2022-12-23 18:31:16 +0000

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,3 +5,8 @@ cd /home/ubuntu/ChatApplication >> /home/ubuntu/ChatApplication/deploy.log
 
 echo 'npm install' >> /home/ubuntu/ChatApplication/deploy.log 
 npm install >> /home/ubuntu/ChatApplication/deploy.log
+export NODE_OPTIONS=--openssl-legacy-provider >> /home/ubuntu/ChatApplication/deploy.log
+cd client >> /home/ubuntu/ChatApplication/deploy.log
+npm run build >> /home/ubuntu/ChatApplication/deploy.log
+rm -rf /var/www/msgme.com/* >> /home/ubuntu/ChatApplication/deploy.log
+cp -r build/*  /var/www/msgme.com/  >> /home/ubuntu/ChatApplication/deploy.log
